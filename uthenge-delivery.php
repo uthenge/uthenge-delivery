e2a36d839d96f98389e5c91cd7707ddfd6e2b430
fshangala <fshangala@gmail.com>
2023-08-17 18:31:41 +0200

    initial settup

---
+++ uthenge-delivery.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package Akismet
+ */
+/*
+Plugin Name: Uthenge Delivery
+Plugin URI: https://github.com/fshangala
+Description: Uthenge plugin for product deliveries
+Version: 1.0
+Requires at least: 5.0
+Requires PHP: 8.1
+Author: Funduluka Shangala
+Author URI: https://linkedin.com/in/fshangala
+License: GPLv2 or later
+Text Domain: uthenge
+*/
